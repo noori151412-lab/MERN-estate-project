4474fc9aa710333a3cde287a8a21f4c81266e4cb
Noori <noori151412@gmail.com>
2026-05-25 21:11:47 +0500

    fix api

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,16 +3,17 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':{
-        target: 'http://localhost:3000',
-        secure: false,
-    },
-  },
-},
   plugins: [
     react(),
     tailwindcss(),
   ],
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
 });
